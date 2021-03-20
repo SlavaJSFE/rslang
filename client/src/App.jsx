47bc4429f6da0a9e7242372fa04000569c3b5323
AStashevskaya <stashevskayaaa@gmail.com>
2021-03-20 18:26:53 +0300

    refactor: delete counter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.scss';
-import Counter from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>RS Lang</h1>
-      <Counter />
     </div>
   );
 }
