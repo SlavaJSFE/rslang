3634a6697a63e1048bce2fb8204487b013c03c9f
AStashevskaya <stashevskayaaa@gmail.com>
2021-03-22 14:17:48 +0300

    feat: add structure of savana game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+import Savanna from './games/Savanna/Savanna';
+
 function App() {
   return (
     <div className="App">
       <h1>RS Lang</h1>
+      <Savanna />
     </div>
   );
 }
---
+++ client/src/games/Savanna/constants.js
@@ -0,0 +1,4 @@
+const NUMBER_OF_WORDS = 5;
+const hello = 'hello';
+
+export { NUMBER_OF_WORDS, hello };
