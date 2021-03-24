5f9defbb21b2751434b2db2c9b73a56e444f810e
Mariya Frantsava <53209068+mariyafrantsava@users.noreply.github.com>
2021-03-24 23:17:09 +0300

    feat: add vocabulary-module with 3 tab-paneles

---
+++ client/src/modules/Vocabulary/DeletedWords/DeletedWords.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DeletedWords() {
+  return (
+    <div>
+      удаленные слова
+    </div>
+  );
+}
--- client/src/modules/Vocabulary/Vocabulary-module.jsx
+++ client/src/modules/Vocabulary/Vocabulary-module.jsx
@@ -1,9 +1,99 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
-export default function VocabularyModule() {
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import DeletedWords from './DeletedWords/DeletedWords';
+import DifficultWords from './DifficultWords/DifficultWords';
+import StudiedWords from './StudiedWords/StudiedWords';
+
+function TabPanel(props) {
+  const {
+    children, value, index, ...other
+  } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="vocabulary-module">
       <h2>Vocabulary Module</h2>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Изучаемые слова" {...a11yProps(0)} />
+          <Tab label="Сложные слова" {...a11yProps(1)} />
+          <Tab label="Удаленные слова" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <StudiedWords />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <DifficultWords />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <DeletedWords />
+        </TabPanel>
+      </div>
     </div>
   );
 }
