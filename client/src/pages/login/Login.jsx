565d7cde1aba0a84bd2d79421a1950cf16d09850
SlavaJSFE <viachaslau.shpileuski@gmail.com>
2021-03-27 13:37:39 +0300

    feat: create login and registaration forms

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,81 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Button,
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header';
+import './Login.scss';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handleClickShowPassword() {
+    setShowPassword(!showPassword);
+  }
+
   return (
     <div className="login-page page">
       <Container>
         <Header />
-        <h2>Login Page</h2>
+        <form
+          className="login-form"
+          onSubmit=""
+        >
+          <h2>Войти в аккаунт</h2>
+          <TextField
+            id="email-input"
+            label="Электронная почта"
+            value={email}
+            type="email"
+            autoComplete="current-email"
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="password-input">Пароль</InputLabel>
+            <OutlinedInput
+              id="password-input"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              endAdornment={(
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              )}
+              labelWidth={60}
+            />
+          </FormControl>
+          <div className="form-button">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Войти
+            </Button>
+          </div>
+        </form>
       </Container>
       <Footer />
     </div>
--- client/src/pages/registration/Registration.jsx
+++ client/src/pages/registration/Registration.jsx
@@ -1,14 +1,78 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header';
+import './Registration.scss';
 
 export default function RegistrationPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handleClickShowPassword() {
+    setShowPassword(!showPassword);
+  }
+
   return (
     <div className="login-page page">
       <Container>
         <Header />
-        <h2>Registration Page</h2>
+        <form className="registration-form">
+          <h2>Создать новый аккаунт</h2>
+          <TextField
+            id="email-input"
+            label="Электронная почта"
+            value={email}
+            type="email"
+            autoComplete="current-email"
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="password-input">Пароль</InputLabel>
+            <OutlinedInput
+              id="password-input"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              endAdornment={(
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              )}
+              labelWidth={60}
+            />
+          </FormControl>
+          <div className="form-button">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Зарегистрироваться
+            </Button>
+          </div>
+        </form>
       </Container>
       <Footer />
     </div>
