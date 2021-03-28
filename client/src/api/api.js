125aeec22ad7ebdb3c4026b9bca9d248e5dc8b67
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2021-03-29 02:24:50 +0300

    fix: correct settings on reload

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,17 +1,13 @@
 /* eslint-disable operator-linebreak */
 import * as axios from 'axios';
+import generateReqBody from './utils';
 
 const userId = '6060e37e33b3c8001500ef3f';
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNjBlMzdlMzNiM2M4MDAxNTAwZWYzZiIsImlhdCI6MTYxNjk2MjQ1NSwiZXhwIjoxNjE2OTc2ODU1fQ.Sv1roL2te5DRUsWi9ZjvLsc5ldmGJEVhwX_8wE_WFEs';
 
-export const updateSettings = async (field, value) => {
-  const reqBody = {
-    wordsPerDay: 5,
-    optional: {
-      [field]: value,
-    },
-  };
+export const updateSettings = async (optional, field, value) => {
+  const reqBody = generateReqBody(optional, field, value);
   try {
     const { data } = await axios.put(
       `https://rslang-server-slavajsfe.herokuapp.com/users/${userId}/settings`,
